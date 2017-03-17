f94361a53502fddc64dfeabf09af0d9bc94eaf8d
Fabian Aussems <fabianaussems@gmail.com>
2017-03-17 18:26:17 +0100

    move re-native to re_native

--- project.clj
+++ project.clj
@@ -1,1 +1,6 @@
-(defproject re-native/action-sheet "0.25.7")
+(defproject re-native/image-picker "0.26.2"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.473"]
+                 [reagent "0.6.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
+                 [re-frame "0.9.1"]]
+  :plugins [[lein-cljsbuild "1.1.4"]])
